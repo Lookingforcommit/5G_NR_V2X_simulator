0b31a5fb2801e7106c4e641413b3f4c6a1b39a2d
Шишкин Артём Вадимович <arvashishkin@edu.hse.ru>
2024-03-01 00:29:04 +0300

    Added functionality for the vehicular blockage check

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class UserInput:
                     if correct:
                         dct[param] = int(dct[param])
                 if not correct:
-                    print(f'Некорректный тип параметра {param}; требуется {self.VALID_PARAMETERS[param]}')
+                    print(f'Некорректный тип параметра {param}, требуется {self.VALID_PARAMETERS[param]}')
                     ans = False
         if not os.path.isfile(dct['dataFilepath']):
             print('Введён некорректный путь к файлу данных симуляции')
@@ -87,7 +87,8 @@ class UserInput:
             correct_params = correct_params and self.check_config_parameters(config_params)
         return correct_params, config_params
 
-    def check_duplicate_columns(self, filename: str, ext: str, configs: Dict[str, Any]) -> bool:
+    @staticmethod
+    def check_duplicate_columns(filename: str, ext: str, configs: Dict[str, Any]) -> bool:
         if ext == '.csv':
             first_row = pd.read_csv(filename, nrows=1, sep=configs['csvSep'])
         else:
@@ -145,6 +146,10 @@ class UserInput:
 class CalcProc:
     LOGNORM_MEAN = 0
     LOGNORM_SIGMA = 3
+    CAR_WIDTH = 1.5  # meters
+    CAR_LENGTH = 4  # meters
+    FLOAT_INACCURACY = 0.01
+    Point = Tuple[float, float]
 
     def __init__(self, conf: Dict[str, Any], sim_data: pd.DataFrame):
         self.cords = sim_data
@@ -158,6 +163,62 @@ class CalcProc:
         self.cords = self.cords.dropna().sort_values(['timestamp', 'vehicle_id'])
         self.cords['vehicle_id'] = self.cords['vehicle_id'].str.replace('.', '_')
         self.cords.set_index(['timestamp', 'vehicle_id'], inplace=True)
+        new_cords = self.cords.apply(lambda x: self.get_full_veh_cords(**x), axis=1, result_type='expand')
+        self.cords = pd.concat([self.cords, new_cords], axis=1)
+
+    def parallel_segments(self, s0_p0: Point, s0_p1: Point, s1_p0: Point, s1_p1: Point) -> bool:
+        if s0_p1[1] == s0_p0[1] or s1_p1[1] == s1_p0[1]:
+            parallel = s0_p1[1] == s0_p0[1] and s1_p1[1] == s1_p0[1]
+        else:
+            k0 = (s0_p1[0] - s0_p0[0]) / (s0_p1[1] - s0_p0[1])
+            k1 = (s1_p1[0] - s1_p0[0]) / (s1_p1[1] - s1_p0[1])
+            parallel = abs(k1 - k0) <= self.FLOAT_INACCURACY
+        return parallel
+
+    def segment_intersection(self, s0_p0: Point, s0_p1: Point, s1_p0: Point, s1_p1: Point) -> bool:
+        parallel = self.parallel_segments(s0_p0, s0_p1, s1_p0, s1_p1)
+        if parallel:
+            p0_dist = np.sqrt((s0_p0[0] - s1_p0[0]) ** 2 + (s0_p0[1] - s1_p0[1]) ** 2)
+            p1_dist = np.sqrt((s0_p1[0] - s1_p0[0]) ** 2 + (s0_p1[1] - s1_p0[1]) ** 2)
+            s0_len = np.sqrt((s0_p1[0] - s0_p0[0]) ** 2 + (s0_p1[1] - s0_p0[1]) ** 2)
+            return abs(p0_dist + p1_dist - s0_len) <= self.FLOAT_INACCURACY
+        else:
+            matrix = np.array([
+                [s0_p1[0] - s0_p0[0], s1_p0[0] - s1_p1[0]],
+                [s0_p1[1] - s0_p0[1], s1_p0[1] - s1_p1[1]]
+            ])
+            solution = np.array([
+                s1_p0[0] - s0_p0[0],
+                s1_p0[1] - s0_p0[1]
+            ])
+            a, b = np.linalg.solve(matrix, solution)
+            ans = 0 <= a <= 1 and 0 <= b <= 1
+            return ans
+
+    def get_full_veh_cords(self, vehicle_x: float, vehicle_y: float, vehicle_angle: float, **kwargs) -> dict:
+        vehicle_angle = 90 - vehicle_angle  # convert to trigonometric
+        if vehicle_angle > 180:
+            vehicle_angle -= 180
+        angle_sin, angle_cos = np.sin(vehicle_angle), np.cos(vehicle_angle)
+        top_l_x = (vehicle_x + self.CAR_LENGTH / 2 * angle_cos - self.CAR_WIDTH / 2 * angle_sin)
+        top_l_y = (vehicle_y + self.CAR_LENGTH / 2 * angle_sin + self.CAR_WIDTH / 2 * angle_cos)
+        top_r_x = (vehicle_x + self.CAR_LENGTH / 2 * angle_cos + self.CAR_WIDTH / 2 * angle_sin)
+        top_r_y = (vehicle_y + self.CAR_LENGTH / 2 * angle_sin - self.CAR_WIDTH / 2 * angle_cos)
+        bot_l_x = (vehicle_x - self.CAR_LENGTH / 2 * angle_cos - self.CAR_WIDTH / 2 * angle_sin)
+        bot_l_y = (vehicle_y - self.CAR_LENGTH / 2 * angle_sin + self.CAR_WIDTH / 2 * angle_cos)
+        bot_r_x = (vehicle_x - self.CAR_LENGTH / 2 * angle_cos + self.CAR_WIDTH / 2 * angle_sin)
+        bot_r_y = (vehicle_y - self.CAR_LENGTH / 2 * angle_sin - self.CAR_WIDTH / 2 * angle_cos)
+        ans = {
+            'top_l_x': top_l_x,
+            'top_l_y': top_l_y,
+            'top_r_x': top_r_x,
+            'top_r_y': top_r_y,
+            'bot_l_x': bot_l_x,
+            'bot_l_y': bot_l_y,
+            'bot_r_x': bot_r_x,
+            'bot_r_y': bot_r_y
+        }
+        return ans
 
     def calc_highway_los_nlosv(self, dists: pd.DataFrame) -> pd.DataFrame:
         lognorm = np.random.lognormal(self.LOGNORM_MEAN, self.LOGNORM_SIGMA, size=dists.shape)
@@ -183,12 +244,38 @@ class CalcProc:
             ans_df = self.calc_highway_los_nlosv(self.dists)
         return ans_df
 
+    def find_los_blockage(self) -> pd.DataFrame:
+        ans_df = pd.DataFrame(False, index=self.dists.index, columns=self.dists.columns)
+        for timestamp, new_df in self.cords.groupby(level=0):
+            new_df = new_df.droplevel(0)
+            rows = new_df.itertuples()
+            for v0 in rows:
+                v0_pos = (v0.vehicle_x, v0.vehicle_y)
+                for v1 in rows:
+                    v1_pos = (v1.vehicle_x, v1.vehicle_y)
+                    if v1[0] != v0[0]:
+                        for block_v in rows:
+                            if block_v[0] != v0[0] and block_v[0] != v1[0]:
+                                bot_left = (block_v.bot_l_x, block_v.bot_l_y)
+                                top_left = (block_v.top_l_x, block_v.top_l_y)
+                                top_right = (block_v.top_r_x, block_v.top_r_y)
+                                bot_right = (block_v.bot_r_x, block_v.bot_r_y)
+                                block_left = self.segment_intersection(v0_pos, v1_pos, bot_left, top_left)
+                                block_top = self.segment_intersection(v0_pos, v1_pos, top_left, top_right)
+                                block_right = self.segment_intersection(v0_pos, v1_pos, bot_right, top_right)
+                                block_bot = self.segment_intersection(v0_pos, v1_pos, bot_left, bot_right)
+                                if any([block_left, block_top, block_right, block_bot]):
+                                    ans_df[v1[0]][timestamp, v0[0]] = True
+                                    break
+        return ans_df
+
     def calc_dists(self) -> pd.DataFrame:
         def transform(df: pd.DataFrame) -> Optional[pd.DataFrame]:
             if df.shape[0] > 1:
                 dists = squareform(pdist(list(zip(df['vehicle_x'], df['vehicle_y']))))
                 new_df = pd.DataFrame(dists, index=df.index.get_level_values(1), columns=df.index.get_level_values(1))
                 return new_df
+
         dists = self.cords.groupby(level=0).apply(transform)
         dists.mask(np.isclose(dists, 0), inplace=True)
         return dists
